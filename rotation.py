fc371018bf0bf40d0cbcc49809eeb818c45f818d
dgketchum <dgketchum@gmail.com>
2019-01-23 10:13:23 -0700

    added plotting

--- rotation.py
+++ rotation.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
+import collections
+from matplotlib import pyplot as plt
+from matplotlib import colors
+import matplotlib.cm as cmx
+from mpl_toolkits.mplot3d import Axes3D
 
 def T(cam_coords):
   return np.matrix([[1,0,0,-cam_coords[0]],[0,1,0,-cam_coords[1]],[0,0,1,-cam_coords[2]],[0,0,0,1]])
@@ -35,6 +40,10 @@ R_pitch = R_pitch(pitch)
 R_roll = R_roll(roll)
 R_axis = R_axis()
 C = R_axis*R_roll*R_pitch*R_yaw*T
+x = []
+y = []
+z = []
+I = []
 
 for index, coord in ene_coords.iterrows():
   np_coord = np.array(coord)
@@ -43,3 +52,18 @@ for index, coord in ene_coords.iterrows():
   x_diff = x_diff[np.newaxis].T
   xyz_coords = C*x_diff
   print(xyz_coords)
+  x.append(xyz_coords[0])
+  y.append(xyz_coords[1])
+  z.append(xyz_coords[2])
+
+z = [float(s) for s in z]
+cm = plt.get_cmap('jet')
+cNorm = colors.Normalize(vmin=min(z), vmax=max(z))
+scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
+fig = plt.figure()
+ax = Axes3D(fig)
+scale = scalarMap.to_rgba(z)
+ax.scatter(x, y, z)
+scalarMap.set_array(z)
+fig.colorbar(scalarMap)
+plt.show()
